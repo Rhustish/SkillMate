f6b12ac1061cb4d2204b79d46e937f111ce5cd40
Rhustish <bbhatnagar27@gmail.com>
2023-05-11 06:26:09 +0530

    homepage setup

---
+++ screens/main/home/infoObj.js
@@ -0,0 +1,14 @@
+export default infoObj={
+    name:"Rohan Misra",
+    age:18,
+    skills:" Guitar , Keyboard , Music Production ",
+    distance: "0.8 KM",
+    proficiency:"Intermediate",
+}
+
+export const connectfunc = ()=>{
+    return 0
+}
+export const rejectfunc = ()=>{
+    return 0
+}
